975e68b7dfed8abdd6b5b0df7bf1dfe66fe61fa7
Gu Zhengxiong <rectigu@gmail.com>
2016-04-18 17:17:48 +0800

    Misc: Fixed the testing script.

--- scripts/should_work.sh
+++ scripts/should_work.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 
-./bintut.py plain -b0.1 "${@}" &&
-    ./bintut.py plain -b0.1 -6 "${@}" &&
-    ./bintut.py nop-slide -b0.1 "${@}" &&
-    ./bintut.py nop-slide -b0.1 -6 "${@}" &&
-    ./bintut.py jmp-esp -b0.1 "${@}" &&
-    ./bintut.py jmp-esp -b0.1 -6 "${@}" &&
-    ./bintut.py ret2lib -b0.1 "${@}" &&
-    ./bintut.py frame-faking -b0.1 "${@}"
+python ./bintut.py plain -b0.1 "${@}" &&
+    python ./bintut.py plain -b0.1 -6 "${@}" &&
+    python ./bintut.py nop-slide -b0.1 "${@}" &&
+    python ./bintut.py nop-slide -b0.1 -6 "${@}" &&
+    python ./bintut.py jmp-esp -b0.1 "${@}" &&
+    python ./bintut.py jmp-esp -b0.1 -6 "${@}" &&
+    python ./bintut.py ret2lib -b0.1 "${@}" &&
+    python ./bintut.py frame-faking -b0.1 "${@}"
